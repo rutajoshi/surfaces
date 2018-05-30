033931a61b40fec569ec209957468dd2259f7ab5
ruta <rjoshi@berkeley.edu>
2018-05-30 16:49:44 -0700

    Use autolab visualization instead of pcl visualizer

--- planar_seg.py
+++ planar_seg.py
@@ -1,5 +1,6 @@
 import numpy as np
 # Point cloud library 
+import pdb
 import pcl
 import pcl.pcl_visualization
 
@@ -8,7 +9,7 @@ import cv2
 
 # Autolab
 from autolab_core import PointCloud
-from perception import DepthImage
+from perception import DepthImage, CameraIntrinsics
 from visualization import Visualizer3D as vis3d
 from visualization import Visualizer2D as vis2d
 from clustering import *
@@ -16,27 +17,37 @@ from clustering import *
 # Load the image into a numpy array
 image = np.load('/nfs/diskstation/projects/dex-net/placing/datasets/real/sample_ims_05_22/depth_ims_numpy/image_000001.npy')
 # Create a DepthImage using the array
-di = DepthImage(image)
-# Visualize the depth image
+ci = CameraIntrinsics.load('/nfs/diskstation/projects/dex-net/placing/datasets/real/sample_ims_05_22/camera_intrinsics.intr')
+di = DepthImage(image, frame=ci.frame)
+pc = ci.deproject(di)
+
+## Visualize the depth image
 vis2d.figure()
 vis2d.imshow(di)
-# Make a dataset of the image
-image = make_dataset(image)
-image = image.astype(np.float32)
+vis2d.show()
+#
+## Visualize the point cloud
+#vis3d.figure()
+#vis3d.points(pc)
+#vis3d.show()
 
-# Viewer
-viewer = pcl.pcl_visualization.CloudViewing()
 
-# Make a PCL type point cloud from the image
-p = pcl.PointCloud()
-p.from_array(image)
-viewer.ShowMonochromeCloud(p)
+# Make and display a PCL type point cloud from the image
+p = pcl.PointCloud(pc.data.T.astype(np.float32))
 
 # Make a segmenter and segment the point cloud.
 seg = p.make_segmenter()
-seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_model_type(pcl.SACMODEL_PARALLEL_PLANE)
 seg.set_method_type(pcl.SAC_RANSAC)
-seg.set_distance_threshold(20)
+seg.set_distance_threshold(0.005)
 indices, model = seg.segment()
 
+#pdb.set_trace()
+vis3d.figure()
+pc_plane = pc.data.T[indices]
+#other_indices = np.array([i for i in range(len(pc.data.T)) if i not in indices])
+#pc_other = pc.data.T[other_indices]
+vis3d.points(pc_plane, color=(1,0,0))
+#vis3d.points(pc_other, color=(0,1,0))
+vis3d.show()
 
