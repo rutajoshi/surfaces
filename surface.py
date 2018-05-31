091ad891b2d81e810172d17b35e0f4e9e528af50
ruta <rjoshi@berkeley.edu>
2018-05-30 17:56:56 -0700

    Add documentation

--- surface.py
+++ surface.py
@@ -8,5 +8,11 @@ class Surface(Object):
         self.normal, self.defining_point = self.point_cloud.best_fit_plane()
 
     def score(self):
+        """ [In progress]
+        Define a metric to score a surface based on how good of a placement location it is.
+        Features used:
+            size_ratio = relative size of this plane compared to other planes available for placement
+            depth = how low is this plane? (lower planes are better)
+        """
         size_ratio = self.size / len(self.parent_cloud)
         return size_ratio
