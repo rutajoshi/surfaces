617786bf534fce9aef587fa5b58431346eb4ad4c
ruta <rjoshi@berkeley.edu>
2018-06-01 14:38:28 -0700

    Visualize the mesh in the most probable stable configuration

--- stable_poses.py
+++ stable_poses.py
@@ -2,10 +2,11 @@ import numpy as np
 import trimesh
 import operator
 
+from autolab_core import RigidTransform
 from visualization import Visualizer2D as vis2d, Visualizer3D as vis3d
 
 # Load the mesh and compute stable poses
-bc = trimesh.load('bar_clamp.obj')
+bc = trimesh.load('demon_helmet.obj')
 stable_poses, probs = bc.compute_stable_poses()
 assert len(stable_poses) == len(probs)
 
@@ -14,8 +15,11 @@ i, value = max(enumerate(probs), key=operator.itemgetter(1))
 best_pose = stable_poses[i]
 print("Most probable pose:")
 print(best_pose)
+print("Probability = " + str(probs[i]))
 
-# Visualize the mesh
-# TODO: visualize mesh in the best stable pose (turn pose into an autolab_core.RigidTransform)
-vis3d.mesh(bc)
+# Visualize the mesh in the most probable stable state
+rotation = np.asarray(best_pose[:3, :3])
+translation = np.asarray(best_pose[:3, 3])
+rt = RigidTransform(rotation, translation, from_frame='obj', to_frame='world')
+vis3d.mesh(bc, rt)
 vis3d.show()
